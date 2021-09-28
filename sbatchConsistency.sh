bef5e845462847b95e682e0ae3912fd64355f380
cfierro94 <constanza.fierro94@gmail.com>
2021-09-28 12:02:18 +0200

    Add sbatch used to test pararel consistency code

--- sbatchConsistency.sh
+++ sbatchConsistency.sh
@@ -5,10 +5,10 @@
 export PYTHONPATH=$(pwd)
 export WORKDIR="/home/wsr217/mpararel"
 
-pararel/consistency/encode_consistency_probe.py \
-        --data_file ${WORKDIR}/data/pararel/trex_lms_vocab/P449.jsonl \
-        --lm 'bert-base-cased' \
-        --graph ${WORKDIR}/data/pararel/pattern_data/graphs/P449.graph \
-        --gpu 0 \
-        --wandb \
-        --use_targets
+python pararel/consistency/encode_consistency_probe.py \
+    --data_file ${WORKDIR}/data/pararel/trex_lms_vocab/P449.jsonl \
+    --lm 'bert-base-cased' \
+    --graph ${WORKDIR}/data/pararel/pattern_data/graphs/P449.graph \
+    --gpu 0 \
+    --wandb \
+    --use_targets
