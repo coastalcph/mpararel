8ad0b99c77247a9daa9d3e82677b9260e1a9c37a
cfierro94 <constanza.fierro94@gmail.com>
2021-10-06 11:24:18 +0200

    Add documentation and formatting to evaluation part of mlama

--- scripts/eval.py
+++ scripts/eval.py
@@ -1,3 +1,4 @@
+"""Compares the correct objects with the predictions stored in result.pkl file."""
 import pickle
 import os
 import numpy as np
@@ -11,51 +12,52 @@ dict_languages_total = {}
 dict_languages_P = {}
 
 for lang in languages:
-  print(lang)
-  P_all = []
-  P_all_eng = []
-  total_all = []
-  relations = list(os.walk(output_path + lang + "/"))[0][1:-1][0]
-  for relation in relations:
-       if "date" in relation:
-           continue
-       P = 0.0
-       P_eng = 0.0
-       total = 0.0
+    print(lang)
+    P_all = []
+    P_all_eng = []
+    total_all = []
+    relations = list(os.walk(output_path + lang + "/"))[0][1:-1][0]
+    for relation in relations:
+        if "date" in relation:
+            continue
+        P = 0.0
+        P_eng = 0.0
+        total = 0.0
 
-       with open(output_path + lang + "/" +  relation + "/" + 'result.pkl', 'rb') as f:
+        with open(output_path + lang + "/" + relation + "/" + 'result.pkl',
+                  'rb') as f:
             data = pickle.load(f)
 
-       with open(path_compare +  relation + "/" + 'result.pkl', 'rb') as f:
+        with open(path_compare + relation + "/" + 'result.pkl', 'rb') as f:
             data_eng = pickle.load(f)
 
-       if len(data["list_of_results"]) >0:
-           eng_dict = {}
-           for d in data_eng["list_of_results"]:
-               rank = 0.0
-               if d['masked_topk']["rank"]==0:
-                   rank = 1.0
-               eng_dict[d["sample"]["uuid"]] = [rank, d["sample"]]
-           for d in data["list_of_results"]:
-               rank = 0.0
-               if d['masked_topk']["rank"]==0:
-                   rank = 1.0
-               P += rank
-               total += 1.0
-               idx = int(d["sample"]["uuid"])
-               if idx in eng_dict:
-                   P_eng += eng_dict[idx][0]
+        if len(data["list_of_results"]) > 0:
+            eng_dict = {}
+            for d in data_eng["list_of_results"]:
+                rank = 0.0
+                if d['masked_topk']["rank"] == 0:
+                    rank = 1.0
+                eng_dict[d["sample"]["uuid"]] = [rank, d["sample"]]
+            for d in data["list_of_results"]:
+                rank = 0.0
+                if d['masked_topk']["rank"] == 0:
+                    rank = 1.0
+                P += rank
+                total += 1.0
+                idx = int(d["sample"]["uuid"])
+                if idx in eng_dict:
+                    P_eng += eng_dict[idx][0]
 
-           P_all.append(P/total)
-           P_all_eng.append(P_eng/total)
-           total_all.append(total)
+            P_all.append(P / total)
+            P_all_eng.append(P_eng / total)
+            total_all.append(total)
 
-  f_out.write(lang)
-  f_out.write(",")
-  f_out.write(str(np.sum(total_all)))
-  f_out.write(",")
-  f_out.write(str(np.mean(P_all)))
-  f_out.write(",")
-  f_out.write(str(np.mean(P_all_eng)))
-  f_out.write("\n")
+    f_out.write(lang)
+    f_out.write(",")
+    f_out.write(str(np.sum(total_all)))
+    f_out.write(",")
+    f_out.write(str(np.mean(P_all)))
+    f_out.write(",")
+    f_out.write(str(np.mean(P_all_eng)))
+    f_out.write("\n")
 f_out.close()
