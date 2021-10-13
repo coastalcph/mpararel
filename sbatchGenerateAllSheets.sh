af6d849f04a9edf9b1418c87c1e8b72f79c152ed
cfierro94 <constanza.fierro94@gmail.com>
2021-10-13 15:03:30 +0200

    Add time to sbatch

--- sbatchGenerateAllSheets.sh
+++ sbatchGenerateAllSheets.sh
@@ -2,12 +2,12 @@
 #SBATCH --job-name=review-sheets
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=0-00:30:00
+#SBATCH --time=0-05:00:00
 
 export PYTHONPATH=$(pwd)
 export WORKDIR="/home/wsr217/mpararel"
 
 python dataset/crowdsourcing/generate_all_sheets.py \
-    --mpararel_folder=$WORKDIR/data/mpararel_00_00_06_02_logging \
+    --mpararel_folder=$WORKDIR/data/mpararel_no_populated \
     --pararel_patterns_folder=$WORKDIR/data/pararel/pattern_data/graphs_json \
-    --language_mapping_file=$WORKDIR/dataset/languages_mapping.txt
+    --language_mapping_file=$WORKDIR/dataset/languages_mapping_2.txt
