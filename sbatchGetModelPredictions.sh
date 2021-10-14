f80973fa9d1840b9c690a594f0f724b9a1815a76
cfierro94 <constanza.fierro94@gmail.com>
2021-10-14 16:30:20 +0200

    decreased the cpus

--- sbatchGetModelPredictions.sh
+++ sbatchGetModelPredictions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=pred_mpararel
-#SBATCH --ntasks=1 --cpus-per-task=20
+#SBATCH --ntasks=1 --cpus-per-task=10
 #SBATCH -p gpu --gres=gpu:titanrtx:1
 
 export PYTHONPATH=$(pwd)
@@ -10,4 +10,4 @@ python evaluate_consistency/get_model_predictions.py \
     --mpararel_folder=$WORKDIR/data/mpararel_no_populated_with_chinese \
     --model_name="bert-base-multilingual-cased" --batch_size=32 \
     --output_folder=$WORKDIR/data/mpararel_predictions/mbert_cased \
-    --cpus 20
+    --cpus 10
