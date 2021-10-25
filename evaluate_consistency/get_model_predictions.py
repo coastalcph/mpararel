6c0cf20b0b80634db5045b1c36f5e30893f3adec
cfierro94 <constanza.fierro94@gmail.com>
2021-10-25 18:06:28 +0200

    Change logging to only 30% of the time

--- evaluate_consistency/get_model_predictions.py
+++ evaluate_consistency/get_model_predictions.py
@@ -309,12 +309,15 @@ def main(args):
             })
         predicted, correct_rank = get_predicted_and_rank_of_correct(
             candidates_to_prob, this_template_tuple.object)
-        LOG.debug(
-            "[{}/{}/{}-{}]\nQueried the model with: '{}'\nConsidered the "
-            "candidates: '{}'\nGot predicted='{}' and rank of correct is '{}'".
-            format(this_template_tuple.language, this_template_tuple.relation,
-                   this_template_tuple.subject, this_template_tuple.object,
-                   inputs, candidates_to_ids.keys(), predicted, correct_rank))
+        if np.random.rand() > 0.7:
+            LOG.debug(
+                "[{}/{}/{}-{}]\nQueried the model with: '{}'\nConsidered the "
+                "candidates: '{}'\nGot predicted='{}' and rank of correct is '{}'"
+                .format(this_template_tuple.language,
+                        this_template_tuple.relation,
+                        this_template_tuple.subject,
+                        this_template_tuple.object, inputs,
+                        candidates_to_ids.keys(), predicted, correct_rank))
         tuples_predictions[this_template_tuple.language][
             this_template_tuple.relation][
                 f"{this_template_tuple.subject}-{this_template_tuple.object}"].append(
